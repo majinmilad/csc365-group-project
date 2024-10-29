5b6574e250c055c3a1f1921eb6387d920857bc3e
majinmilad <mchabok@calpoly.edu>
2024-10-28 19:52:22 -0700

    test

--- main.py
+++ main.py
@@ -14,5 +14,6 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 @app.post("/testing/{item_id}")
-def read_item():
-    return {"item_id": item_id, "q": q}
+def read_item(num: int):
+    print('testing output')
+    return {"item_id": 333}
