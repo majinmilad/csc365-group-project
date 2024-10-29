ea191353e48d1787c2e20c11ed8949560c774d55
majinmilad <mchabok@calpoly.edu>
2024-10-28 19:42:07 -0700

    test

--- main.py
+++ main.py
@@ -12,3 +12,7 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/testing/{item_id}")
+def read_item():
+    return {"item_id": item_id, "q": q}
