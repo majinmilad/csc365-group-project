48e6e62493d36d04f70f63aa58ef0baef1d85437
majinmilad <mchabok@calpoly.edu>
2024-10-28 22:34:12 -0700

    push working endpoints for playlist

--- main.py
+++ main.py
@@ -13,15 +13,6 @@ engine = create_engine(DATABASE_URL)
 async def root():
     return {"message": "Changing this message! Whoo!"}
 
-@app.get("/items/{item_id}")
-def test_read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-@app.post("/testing/{item_id}")
-def test_function(num: int): # parameters show up in docs
-    print('testing output') # prints show up in render log
-    return {"item_id": 333} # returns act as responses
-
 @app.post("/user/{user_id}/playlist/create_playlist")
 def create_playlist(user_id: int, playlist_name: str = None):
     
@@ -40,6 +31,19 @@ def create_playlist(user_id: int, playlist_name: str = None):
 
     return playlist_id
 
+@app.post("/user/{user_id}/playlist/add_song")
+def add_song_to_playlist(user_id: int, song_id: int, playlist_id: int):
+
+    with engine.begin() as connection:
+
+        # insert a new playlist into table
+        sql_to_execute = 'INSERT INTO playlist_song (song_id, playlist_id) VALUES (:song_id, :playlist_id)'
+        connection.execute(sqlalchemy.text(sql_to_execute), {'song_id': song_id, 'playlist_id': playlist_id})
+    
+    return "Ok"
+
 
 if __name__=="__main__":
-    create_playlist(user_id=24, playlist_name='dope ass playlist')
+    # create_playlist(user_id=24, playlist_name='dope ass playlist')
+    # add_song_to_playlist(3, 4, 5)
+    pass
