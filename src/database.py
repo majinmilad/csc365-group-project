6d21f6644f02d199073541013ee9778caf229e6e
Jackson McLaughlin <jacksondmclaughlin@gmail.com>
2024-12-03 18:25:21 -0800

    Testing for call ms

--- src/database.py
+++ src/database.py
@@ -5,9 +5,12 @@ from sqlalchemy import create_engine
 
 def database_connection_url():
     dotenv.load_dotenv()
-    return os.environ.get("POSTGRES_URI")
-
-DATABASE_URL = "postgresql://postgres.nqueeijmuxlkmckrcxst:weresocookedbro@aws-0-us-west-1.pooler.supabase.com:6543/postgres"
+    DB_USER: str = os.environ.get("POSTGRES_USER")
+    DB_PASSWD = os.environ.get("POSTGRES_PASSWORD")
+    DB_SERVER: str = os.environ.get("POSTGRES_SERVER")
+    DB_PORT: str = os.environ.get("POSTGRES_PORT")
+    DB_NAME: str = os.environ.get("POSTGRES_DB")
+    return f"postgresql://{DB_USER}:{DB_PASSWD}@{DB_SERVER}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(database_connection_url(), pool_pre_ping=True)
 
 metadata_obj = sqlalchemy.MetaData()
