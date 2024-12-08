8a368683a4321a418a7be676209e6743f4ac5ed2
Jackson McLaughlin <jacksondmclaughlin@gmail.com>
2024-12-08 13:33:49 -0800

    Made popular songs more efficient

--- src/api/analytics.py
+++ src/api/analytics.py
@@ -37,22 +37,18 @@ def most_popular_playlists():
 def most_popular_songs():
     with db.engine.begin() as connection:
         sql_query = sqlalchemy.text("""
-            WITH Ranked_songs AS
+            WITH song_count AS
             (
-                SELECT song.song_id as id, song.title, album.title as album_name, artist.name as artist_name, playlist_song.playlist_id, COUNT(playlist_song.playlist_id) OVER (PARTITION BY song.song_id) AS saves
-                FROM song
-                LEFT JOIN playlist_song ON playlist_song.song_id = song.song_id
-                JOIN album ON album.album_id = song.album_id
-                JOIN artist ON artist.artist_id = song.artist_id
-            ),
-            Distinct_songs AS
-            (
-                SELECT DISTINCT Saves, Ranked_songs.title, album_name, artist_name, Ranked_Songs.id
-                FROM Ranked_Songs
-                LIMIT 100
+                SELECT song_id as id, COUNT(playlist_song.playlist_id) AS saves
+                FROM playlist_song
+                GROUP BY song_id
+                ORDER BY COUNT(playlist_song.playlist_id)
             )
-            SELECT RANK() OVER (ORDER BY saves DESC, title ASC) AS Rnk, title, album_name, artist_name, id
-            FROM Distinct_songs
+            SELECT RANK() OVER (ORDER BY saves DESC, song.title ASC) AS Rnk, song.title AS title, album.title AS album_name, artist.name AS artist_name, id
+            FROM song_count
+            LEFT JOIN song ON id = song.song_id
+            LEFT JOIN artist ON song.artist_id = artist.artist_id
+            LEFT JOIN album ON song.album_id = album.album_id
             LIMIT 100
             """)
         songs = connection.execute(sql_query).fetchall()
